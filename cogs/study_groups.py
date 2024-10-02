a98e958d689eadbc431199ea9bf3d9602142cc4b
FÆZEN <107471714+faezen@users.noreply.github.com>
2024-10-03 01:08:57 +0530

    Update study_groups.py

--- cogs/study_groups.py
+++ cogs/study_groups.py
@@ -1,6 +1,5 @@
 import discord
 from discord import app_commands
-from discord.ext import commands
 import asyncio
 from utils import parse_seconds_to_hms, parse_mentions, validate_parameters
 import logging
